7bbd0933110dd94ec369a46f69dd0710518507fc
Tzion <tziondebela@gmail.com>
2022-11-21 20:19:35 +0200

    update EX2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Counter from './Counter';
 function App() {
   return (
     <div className="App">
-      <h1>Exercise</h1>
-      <Company company_name="Hello" num_of_employees="56" company_address="Jerusalem, Yaffo 5" />
       <Counter></Counter>
     </div>
   );
