477c103d4ec1c9ca2e45365653f6d87b00877269
Tzion <tziondebela@gmail.com>
2022-11-28 21:13:10 +0200

    add num input

--- src/Counter.js
+++ src/Counter.js
@@ -1,24 +1,28 @@
 import{useState} from"react";
 function Counter() {
     const [count, setCount] = useState(1)
+    const[delta,setDelta]=useState(1)
+
     function incr(){
         setCount(
             function(oldcount){
-                return oldcount+1
+                return oldcount+delta
             }
         )
     }
     function reset(){
-        setCount(
-            function(){
-                return 1
-            }
-        )
+        setCount(0)
     }
+    function handleDelta(e){
+        setDelta(Number(e.target.value))
+    }
+
     return (
         <div>
-            <h1>count is at {count}</h1>
-            <button onClick={incr}>Click to add 1 to counter</button>
+            <h1>Counter</h1>
+            <input type="number" value={delta} onChange={handleDelta}></input>
+            <h3>counter is at {count}</h3>
+            <button onClick={incr}>Click to add {delta} to counter</button>
             <p/>
             <button onClick={reset}>Click to reset counter</button>
         </div>
